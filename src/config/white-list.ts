550f91129da2c75d0863ebccd9ce50670fb74499
Neal-QC <neal-qc@gate.me>
2024-04-09 09:12:21 +0800

    update white-list

--- src/config/white-list.ts
+++ src/config/white-list.ts
@@ -1,7 +1,7 @@
 import { type RouteLocationNormalized } from "vue-router"
 
 /** 免登录白名单（匹配路由 path） */
-const whiteListByPath: string[] = ["/login","/cards","newcards"]
+const whiteListByPath: string[] = ["/login","/cards","/newcards"]
 
 /** 免登录白名单（匹配路由 name） */
 const whiteListByName: string[] = []
